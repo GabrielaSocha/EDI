b4157c931adf82afa3df482c9a6b78b365d12c23
GabrielaSocha <232490@student.uek.krakow.pl>
2023-12-29 23:27:34 +0100

    Sekcja czlonkow zespolu, wczytanie danych do tabeli, przyciete zdjecia

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var members = document.querySelectorAll('.member');
 
-    members.forEach(function (member) {
-        member.addEventListener('click', function () {
-            this.querySelector('.member-details').classList.toggle('show-details');
-        });
+function populateTable(data) {
+    const tableBody = document.querySelector('#data-table tbody');
+
+    tableBody.innerHTML = '';
+
+    data.forEach(item => {
+        const row = tableBody.insertRow();
+        const idCell = row.insertCell(0);
+        const brandCell = row.insertCell(1);
+        const yearCell = row.insertCell(2);
+        const modelCell = row.insertCell(3);
+        const colorCell = row.insertCell(4);
+        const vinCell = row.insertCell(5);
+
+        idCell.textContent = item.id;
+        brandCell.textContent = item.make;
+        yearCell.textContent = item.year;
+        modelCell.textContent = item.model;
+        colorCell.textContent = item.color;
+        vinCell.textContent = item.vin;
+
     });
-});
+}
