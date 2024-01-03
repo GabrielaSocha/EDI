5aa3f3881a59f15166b4761fca21ee6b44b59ecd
WojtowiczZuzanna <zuzia.wojtowicz2004@gmail.com>
2024-01-03 20:39:15 +0100

    przeniesione scripty

--- script.js
+++ script.js
@@ -49,3 +49,122 @@ function centerChart(chart) {
     chart.style.display = "block";
     chart.style.margin = "0 auto";
 }
+
+//Chart-1
+fetch('https://my.api.mockaroo.com/cars.json?key=26099330')
+  .then(response => response.json())
+  .then(data => {
+    const productionYearsCount = {};
+
+    data.forEach(entry => {
+      const year = entry.year;
+      productionYearsCount[year] = (productionYearsCount[year] || 0) + 1;
+    });
+
+    const labels = Object.keys(productionYearsCount);
+    const dataValues = Object.values(productionYearsCount);
+
+    const ctx = document.getElementById('carYearsChart').getContext('2d');
+    const carYearsChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Number of cars',
+          data: dataValues,
+          backgroundColor: '#9966FF', 
+          borderColor: '#6633CC', 
+          borderWidth: 2, 
+        }],
+      },
+      options: {
+        plugins:{
+          legend:{
+            labels, title: {
+              color: 'white'
+            }
+          }
+        },
+
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+              color: 'white'
+            },
+            title: {
+              display: true,
+              text: 'Number of cars', color: 'white'
+            },
+          },
+          x: {
+            ticks: {
+              color: 'white'
+            },
+            title: {
+              display: true,
+              text: 'Production year', color: 'white'
+            },
+          },
+        },
+        responsive: false,
+        maintainAspectRatio: true
+      },
+    });
+  })
+
+  .catch(error => console.error('Error fetching data:', error));
+
+
+//Chart-2
+fetch('https://my.api.mockaroo.com/cars.json?key=26099330')
+    .then(response => response.json())
+    .then(data => {
+      const carMakesCount = {};
+
+      data.forEach(entry => {
+        const make = entry.make;
+        carMakesCount[make] = (carMakesCount[make] || 0) + 1;
+      });
+
+      const labels = Object.keys(carMakesCount);
+      const dataValues = Object.values(carMakesCount);
+
+      const options = {
+        plugins:{
+          legend:{
+            labels: {
+              color: 'white'
+            }
+          }
+        },
+        responsive: false,
+        maintainAspectRatio: true
+      };
+      const ctx = document.getElementById('carMakesChart').getContext('2d');
+
+      const carMakesChart = new Chart(ctx, {
+        scaleFontColor: "white",
+        type: 'pie',
+        data: {
+          labels: labels,
+          datasets: [{
+            data: dataValues,
+            backgroundColor: getRandomColors(dataValues.length),
+            borderColor: '#D3D3D3'
+          }],
+        },
+        options: options
+      });
+
+      function getRandomColors(count) {
+        const colors = [];
+        for (let i = 0; i < count; i++) {
+          const color = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.7)`;
+          colors.push(color);
+        }
+        return colors;
+      }
+    })
+    
+    .catch(error => console.error('Error fetching data:', error));
